5be64d662f4aafd0060c74cfea747bd6095db934
byelipk <byelipk@gmail.com>
2016-08-26 14:22:23 -0400

    Imrpove test coverage of user model

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule HelloEcto.UserTest do
   use HelloEcto.ModelCase
 
@@ -5,6 +7,10 @@ defmodule HelloEcto.UserTest do
 
   @valid_attrs %{bio: "content", email: "email@example.com", name: "some content", number_of_pets: 42}
   @invalid_attrs %{}
+
+  def long_string(length) do
+    Enum.reduce (1..length), "", fn(_, acc) -> acc <> "a" end
+  end
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
@@ -14,5 +20,25 @@ defmodule HelloEcto.UserTest do
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "number_of_pets is not required" do
+    changeset = User.changeset(%User{}, Map.delete(@valid_attrs, :number_of_pets))
+    assert changeset.valid?
+  end
+
+  test "bio must be at least two characters long" do
+    attrs = %{@valid_attrs | bio: "I"}
+    assert {:bio, "should be at least 2 character(s)"} in errors_on(%User{}, attrs)
+  end
+
+  test "bio cannot be more than 10 characters long" do
+    attrs = %{@valid_attrs | bio: long_string(11)}
+    assert {:bio, "should be at most 10 character(s)"} in errors_on(%User{}, attrs)
+  end
+
+  test "email must contain at least an @" do
+    attrs = %{@valid_attrs | email: "example.com"}
+    assert {:email, "has invalid format"} in errors_on(%User{}, attrs)
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -6,19 +6,22 @@ defmodule HelloEcto.User do
     field :email, :string
     field :bio, :string
     field :number_of_pets, :integer
-    
+
     has_many :videos, HelloEcto.Video
 
     timestamps()
   end
 
+  @required_fields ~w(name email bio)
+  @optional_fields ~w(number_of_pets)
+  
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :email, :bio, :number_of_pets])
-    |> validate_required([:name, :email, :bio, :number_of_pets])
+    |> validate_required([:name, :email, :bio])
     |> validate_length(:bio, min: 2)
     |> validate_length(:bio, max: 10)
     |> validate_format(:email, ~r/@/)
